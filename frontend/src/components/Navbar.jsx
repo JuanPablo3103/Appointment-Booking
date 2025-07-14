1a6caa0401b27ffcf7a9db47e57c84b7cd52af65
Juan Castro <juanp.castrom@unac.edu.co>
2025-07-14 00:34:59 -0500

    💥 Add new commit - create banner

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import { assets } from '../assets/assets_frontend/assets'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
+            <img className='w-44 cursor-pointer' src={assets.logo} alt='' />
+            <ul className="hidden md:flex items-start gap-5 font-medium">
+                <NavLink to='/'>
+                    <li className='py-1'>HOME</li>
+                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+                </NavLink>
+                <NavLink to='/barbers'>
+                    <li className='py-1'>ALL BARBERS</li>
+                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+                </NavLink>
+                <NavLink to='/about'>
+                    <li className='py-1'>ABOUT</li>
+                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+                </NavLink>
+                <NavLink to='/contact'>
+                    <li className='py-1'>CONTACT</li>
+                    <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+                </NavLink>
+            </ul>
+            <div>
+                <button className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
